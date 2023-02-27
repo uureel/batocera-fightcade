bf04ace556dc86fa8cdfccd3e1c96ec531b20706
uureel <116395185+uureel@users.noreply.github.com>
2023-02-28 00:26:36 +0100

    +

--- installer/fightcade.sh
+++ installer/fightcade.sh
@@ -9,15 +9,15 @@
 clear; echo
 kernel=$(uname -a | awk '{print $3}' 2>/dev/null)
 if [[ "$kernel" < "5.18" ]]; then 
-echo -e "${RED}ERROR: THIS SYSTEM IS NOT SUPPORTED"
-echo -e "${RED}YOU NEED BATOCERA VERSION 35+"
+echo -e "ERROR: THIS SYSTEM IS NOT SUPPORTED"
+echo -e "YOU NEED BATOCERA VERSION 35+"
 sleep 3
 exit 0; exit 1; exit 2
 fi 
 free=$(df -h | grep "userdata" | awk '{print $4}' | sed 's,G,,g' | cut -d "." -f1)
 if [[ "$free" -le "4" ]]; then 
-echo -e "${RED}ERROR: YOU NEED AT LEAST 4GB OF FREE DISK SPACE ON /USERDATA "
-echo -e "${RED}YOU HAVE $free GB"
+echo -e "ERROR: YOU NEED AT LEAST 4GB OF FREE DISK SPACE ON /USERDATA "
+echo -e "YOU HAVE $free GB"
 sleep 3
 exit 0; exit 1; exit 2
 fi 
