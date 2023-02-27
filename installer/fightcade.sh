c5a1dd54c5beb3b985e2d4b75307e867dda9250e
uureel <116395185+uureel@users.noreply.github.com>
2023-02-27 20:28:42 +0100

    +

--- installer/fightcade.sh
+++ installer/fightcade.sh
@@ -24,6 +24,24 @@ fi
 #
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+#
+spinner()
+{
+    local pid=$1
+    local delay=0.2
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf "PLEASE WAIT . . .  %c   " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+    done
+    printf "   \b\b\b\b"
+}
+#
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+#\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 # prepare paths and files for installation 
 cd ~/
 killall fc2-electron 2>/dev/null
@@ -32,11 +50,10 @@ tmp=/tmp/batocera-fightcade; rm -rf $tmp 2>/dev/null
 # --------------------------------------------------------------------
 # -- prepare dependencies for this app and the installer: 
 url=https://raw.githubusercontent.com/uureel/batocera-fightcade/main/installer
+wget -q -O $tmp/installer.sh $url/fightcade.sh
+dos2unix $tmp/installer.sh 2>/dev/null; chmod a+x $tmp/installer.sh 2>/dev/null
 wget -q -O /lib/libselinux.so.1 $url/libselinux.so.1 2>/dev/null 
-wget -q -O /lib/libtinfo.so.6 $url/libtinfo.so.6 2>/dev/null 
-wget -q -O /usr/bin/tput $url/tput 2>/dev/null 
-wget -q -O /bin/tar $url/tar 2>/dev/null 
-chmod a+x /bin/tar 2>/dev/null; chmod a+x /usr/bin/tput 2>/dev/null
+wget -q -O /bin/tar $url/tar 2>/dev/null; chmod a+x /bin/tar 2>/dev/null
 # --------------------------------------------------------------------
 # show console info: 
 clear
@@ -84,8 +101,42 @@ echo -e "DOWNLOADING FIGHTCADE [8/9] . . ."
 curl --progress-bar --remote-name --location https://github.com/uureel/batocera-fightcade/raw/main/package/fightcade.tar.gz.partah
 echo -e "DOWNLOADING FIGHTCADE [9/9] . . ."
 curl --progress-bar --remote-name --location https://github.com/uureel/batocera-fightcade/raw/main/package/fightcade.tar.gz.partai
-size=$(du -h ~/pro/fightcade/extras/downloads | tail -n 1 | awk '{print $1}' | sed 's,G,,g')
-echo -e "DONE, $size GB"
+#
+# check downloads integrity 
+p1=$dl/fightcade.tar.gz.partaa
+p2=$dl/fightcade.tar.gz.partab
+p3=$dl/fightcade.tar.gz.partac
+p4=$dl/fightcade.tar.gz.partad
+p5=$dl/fightcade.tar.gz.partae
+p6=$dl/fightcade.tar.gz.partaf
+p7=$dl/fightcade.tar.gz.partag
+p8=$dl/fightcade.tar.gz.partah
+p9=$dl/fightcade.tar.gz.partai
+if [[ -f "$p1" ]] && [[ -f "$p2" ]] && [[ -f "$p3" ]] && [[ -f "$p4" ]] && [[ -f "$p5" ]] && [[ -f "$p6" ]] && [[ -f "$p7" ]] && [[ -f "$p8" ]] && [[ -f "$p9" ]]; 
+    then 
+    p1m=$(md5sum $p1 | awk '{print $1}')
+    p2m=$(md5sum $p2 | awk '{print $1}')
+    p3m=$(md5sum $p3 | awk '{print $1}')
+    p4m=$(md5sum $p4 | awk '{print $1}')
+    p5m=$(md5sum $p5 | awk '{print $1}')
+    p6m=$(md5sum $p6 | awk '{print $1}')
+    p7m=$(md5sum $p7 | awk '{print $1}')
+    p8m=$(md5sum $p8 | awk '{print $1}')
+    p9m=$(md5sum $p9 | awk '{print $1}')
+    if [[ "$p1m" = "38e21dc62048b23cbe43d6de0914f97f" ]] && [[ "$p2m" = "5c601b1608990d12191ae33d301b2ca0" ]] && [[ "$p3m" = "12ccf39c925d1e12783acae2aa957490" ]] && [[ "$p4m" = "0381e60bf464ae2796ccbc50e60a9488" ]] && [[ "$p5m" = "2b0ac3ed77b469e2c2fcc983707a824c" ]] && [[ "$p6m" = "8ec9ec1d0a53718f2a89fc43c90d275a" ]] && [[ "$p7m" = "4bda7b29153e3445fe23df4b1a4b5707" ]] && [[ "$p8m" = "e2cfec24628e1a63c2232e79a81251e6" ]] && [[ "$p9m" = "8a94799487624e81c3419df0346b2450" ]]; 
+        then 
+            #
+            size=$(du -h ~/pro/fightcade/extras/downloads | tail -n 1 | awk '{print $1}' | sed 's,G,,g')
+            echo -e "DONE, $size GB"
+        else 
+            echo
+            echo -e "DOWNLOAD WENT BAD! ;( "
+            sleep 2
+            echo -e "RESTARTING INSTALLER . . ."
+            sleep 3
+            exit 0 & /tmp/batocera-fightcade/installer.sh 
+    fi
+fi
 #
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
@@ -99,12 +150,10 @@ echo -e "DONE"
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 #
-echo
-echo -e "EXTRACTING . . ."
+echo -e "EXTRACTING. . . ."
 cd /userdata/system/pro/
 mv /userdata/system/pro/fightcade/extras/downloads/fightcade.tar.gz /userdata/system/pro/
-tar -xf /userdata/system/pro/fightcade.tar.gz
-tar -xf /userdata/system/pro/fightcade.tar.gz 2>/dev/null
+tar -xf /userdata/system/pro/fightcade.tar.gz & spinner $! 
 rm -rf /userdata/system/pro/fightcade/extras/downloads 2>/dev/null
 size=$(du -h ~/pro/fightcade | tail -n 1 | awk '{print $1}' | sed 's,G,,g')
 echo -e "$size GB"
